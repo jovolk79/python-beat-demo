0bcf63272cddf49692b6d87b58d234e1ba9bab0b
patter5 <jeffreypine@gmail.com>
2017-10-22 23:36:52 -0700

    added check for wordnet data, refactored

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-before running, issue command:
+## Setup
 
+1. Create and activate virtual environment in the working directory:
+```
+> virtualenv venv <path-to-python3.x>
+> . venv/bin/activate
+```
+
+2. Install the requirements
 ```
 pip install -r requirements.txt
 ```
-Better yet, create a virtual environment and activate before installing the
-requirements.
--- nltk_demo.py
+++ nltk_demo.py
@@ -1,27 +1,32 @@
 from nltk.corpus import wordnet as wn
-
 import nltk
-
 import random
 
-from nltk.corpus import stopwords as sw
+try:
+    # Check to see if data already exists
+    nltk.data.find('corpora/wordnet')
+    print('Wordnet data found at ' + str(nltk.data.path[len(nltk.data.path)-1]))
+except LookupError:
+    # if data doesn't exist, download it
+    print('Wordnet data needs to be downloaded...')
+    nltk.download('wordnet')
 
-#nltk.download('wordnet')
-
-# dictionary to hold parts of speech abbreviations
+# dictionary to hold parts of speech by abbreviations
+"""
 POS = {
-        'v': 'verb',
-        'a': 'adjective',
-        'n': 'noun',
-        'r': 'adverb'
-        }
-
+    'v': 'verb',
+    'a': 'adjective',
+    'n': 'noun',
+    'r': 'adverb'
+}
+"""
 
 nouns = []
 verbs = []
 adjectives = []
 adverbs = []
 
+verbose = True
 
 def strip_underscores(elts):
     temp = elts.name().split('_')
@@ -35,62 +40,68 @@ def strip_underscores(elts):
 
 
 def pop_nouns():
-
+    count = 0
     for ss in list(wn.all_synsets('n')):
         for elts in ss.lemmas():
+            count += 1
             # print(elts.name())
             if '_' in elts.name():
                 temp_word = strip_underscores(elts)
                 nouns.append(temp_word)
             else:
                 nouns.append(elts.name())
-
-
-
+    print(str(count) + ' nouns loaded.')
 
 
 def pop_verbs():
-
+    count = 0
     for ss in list(wn.all_synsets('v')):
+        count += 1
         for elts in ss.lemmas():
-            #print(elts.name())
+            # print(elts.name())
             if '_' in elts.name():
                 temp_word = strip_underscores(elts)
                 verbs.append(temp_word)
             else:
                 verbs.append(elts.name())
+    print(str(count) + ' verbs loaded.')
 
 
 def pop_adj():
-
+    count = 0
     for ss in list(wn.all_synsets('a')):
+        count += 1
         for elts in ss.lemmas():
             if '_' in elts.name():
                 adjectives.append(strip_underscores(elts))
             else:
                 adjectives.append(elts.name())
+    print(str(count) + ' adjectives loaded')
+
 
 def pop_adverbs():
-
+    count = 0
     for ss in list(wn.all_synsets('r')):
         for elts in ss.lemmas():
+            count += 1
             if '_' in elts.name():
                 adverbs.append(strip_underscores(elts))
             else:
                 adverbs.append(elts.name())
+    print(str(count) + ' adverbs loaded.')
+
+
+def setup():
+    print('Loading dictionaries...')
+    pop_adverbs()
+    pop_adj()
+    pop_nouns()
+    pop_verbs()
 
 
 def get_rand_of_type(pos_list):
     this_list = pos_list
     return this_list[random.randrange(len(this_list))]
-
-# list_of_english_stopwords = sw.words('english')
-
-# print(sw.words('english'))
-
-# print(len(list_of_english_stopwords))
-
-# print(list_of_english_stopwords[random.randrange(len(list_of_english_stopwords))])
 
 
 def beat_model():
@@ -101,7 +112,7 @@ def beat_model():
             adverb noun verb
     :return: beat string
     """
-    poem = ''\
+    poem = '' \
            + get_rand_of_type(adjectives).capitalize() \
            + ' ' + get_rand_of_type(nouns) \
            + ' ' + get_rand_of_type(verbs) + '\n'
@@ -111,26 +122,17 @@ def beat_model():
             + get_rand_of_type(verbs) + '\n'
 
     poem += get_rand_of_type(nouns).capitalize() + ', ' \
-            + get_rand_of_type(nouns) + ' —' \
+            + get_rand_of_type(nouns) + ' ' \
             + get_rand_of_type(verbs) + '.\n'
 
     return poem
 
 
-pop_nouns()
-pop_verbs()
-pop_adj()
-pop_adverbs()
+def display_poem():
+    print('\nHere\'s your beat poem:\n')
+    print(beat_model())
 
-print('Num verbs: ' + str(len(verbs)))
-print('Num nouns: ' + str(len(nouns)))
-print('Num adj: '   + str(len(adjectives)))
-print('Num adv: '   + str(len(adverbs)))
-print(verbs[random.randrange(len(verbs))])
-print(get_rand_of_type(nouns) + '\n\n')
+if __name__ == "__main__":
 
-print(beat_model())
-
-
-
-
+    setup()
+    display_poem()
